174e8808c0aa2e9b39c2ed8fdf2ad31489cde5ab
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-04-21 21:16:37 -0600

    Add files via upload

---
+++ alien_colours.py
@@ -0,0 +1,88 @@
+
+alien_color = 'green'
+
+
+if 'green' in alien_color:
+	print("You have just earned 5 points! ")
+
+
+
+
+alien_color = 'green'
+
+
+if 'red' in alien_color:
+	print("You have just earned 5 points! ")
+
+
+
+print('_________________________')
+#Alien_color #2
+
+alien_color = 'orange'
+
+if 'red' in alien_color:
+	print("You have just earned 5 points!")
+else:
+	print("You have just earned 10 points!")
+
+
+#Writing the same program that runs the if block.
+
+alien_color = 'orange'
+
+if 'orange' in alien_color:
+	print("\nYou have just earned 5 points!")
+else:
+	print("\nYou have just earned 10 points!")
+
+
+
+
+print("________________________________________")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+	print("You have just earned 5 points!")
+elif alien_color == 'yellow':
+	print("You have just earned 10 points!")
+else:
+	print("You just earned 15 points")
+
+
+
+
+
+
+
+alien_color = 'green'
+
+if alien_color == 'green':
+	print("You have just earned 5 points!")
+elif alien_color == 'yellow':
+	print("You have just earned 10 points!")
+else:
+	print("You just earned 15 points")
+
+
+
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+	print("You have just earned 5 points!")
+elif alien_color == 'yellow':
+	print("You have just earned 10 points!")
+else:
+	print("You just earned 15 points")
+
+
+
+
+
+
+
+
+
+
